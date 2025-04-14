9902107c80f7f2d3ff474b8746e0b92fcc581a6e
Louis-012 <lpanglewai12@gmail.com>
2025-04-14 17:16:11 +0700

    add sign in sign up schema

--- src/view/sign-in/page.tsx
+++ src/view/sign-in/page.tsx
@@ -1,10 +1,29 @@
+"use client";
 import * as React from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import { Form, Formik, FormikProps } from "formik";
+import { SignInSchema } from "./schema/signInSchema";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const SignIn = () => {
+  interface IUserValue {
+    email: string;
+    password: string;
+  }
+
+  const [typePass, setTypePass] = React.useState<string>("password");
+
+  const onBtShowPass = () => {
+    if (typePass === "password") {
+      setTypePass("text");
+    } else if (typePass === "text") {
+      setTypePass("password");
+    }
+  };
+
   return (
     <div className="bg-gradient-to-r from-[#8B5CF6] to-[#3B82F6] flex rounded h-screen py-12 text-black p-3">
       <div className="w-full md:w-1/3 m-auto h-fit rounded-lg shadow-xl">
@@ -13,23 +32,79 @@ const SignIn = () => {
             <h1 className="text-2xl font-bold">Sign In</h1>
           </CardHeader>
           <CardContent>
-            <div className="py-2 md:py-6 space-y-5">
-              <Input name="email" type="email" placeholder="Email" />
-              <div className="flex items-center justify-between ">
-                <Input name="password" placeholder="Password" />
-              </div>
-              <div className="flex items-center justify-between ">
-                <Input name="confPassword" placeholder="Confirm Password" />
-              </div>
-              <div className="flex gap-4">
-                <Button type="submit" className="text-white">
-                  Sign In
-                </Button>
-              </div>
-            </div>
-            <Link type="button" href={"/sign-up"} className="font-bold">
-              No Account?
-            </Link>
+            <Formik
+              initialValues={{ email: "", password: "" }}
+              validationSchema={SignInSchema}
+              onSubmit={(values) => {
+                console.log(values);
+                //onSignIn function
+              }}
+            >
+              {(props: FormikProps<IUserValue>) => {
+                const { errors, values, handleChange } = props;
+                console.log("Error from yup", errors);
+                return (
+                  <Form>
+                    <div className="py-2 md:py-6 space-y-5">
+                      <Input
+                        name="email"
+                        type="email"
+                        placeholder="Email"
+                        onChange={handleChange}
+                        value={values.email}
+                      />
+                      <div className="flex items-center justify-between ">
+                        <Input
+                          name="password"
+                          type={typePass}
+                          placeholder="Password"
+                          onChange={handleChange}
+                          value={values.password}
+                        />
+                        <Button
+                          type="button"
+                          className="shadow-none p-0"
+                          onClick={onBtShowPass}
+                        >
+                          {typePass === "password" ? (
+                            <FaEyeSlash size={24} />
+                          ) : (
+                            <FaEye size={24} />
+                          )}
+                        </Button>
+                      </div>
+                      <div className="flex items-center justify-between ">
+                        <Input
+                          name="confPassword"
+                          type={typePass}
+                          placeholder="Confirm Password"
+                          onChange={handleChange}
+                        />
+                        <Button
+                          type="button"
+                          className="shadow-none p-0"
+                          onClick={onBtShowPass}
+                        >
+                          {typePass === "password" ? (
+                            <FaEyeSlash size={24} />
+                          ) : (
+                            <FaEye size={24} />
+                          )}
+                        </Button>
+                      </div>
+                      <div className="flex gap-4">
+                        <Button type="submit" className="text-white">
+                          Sign In
+                        </Button>
+                      </div>
+                    </div>
+                    <Link type="button" href={"/sign-up"} className="font-bold">
+                      No Account?
+                    </Link>
+                  </Form>
+                );
+              }}
+            </Formik>
           </CardContent>
         </Card>
       </div>
