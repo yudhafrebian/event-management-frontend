1a2d696378b1be3d3a97529e42aa5a51627c4e97
Yudha Frebian <anandayudha777@gmail.com>
2025-04-28 09:12:45 +0700

    feat: update event details and ticket handling; refactor AboutSection props and enhance Navbar routing

--- src/view/events/section/TicketTypes.tsx
+++ src/view/events/section/TicketTypes.tsx
@@ -2,6 +2,9 @@ import { CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import * as React from "react";
 import Link from "next/link";
+import { useAppSelector } from "@/app/hook";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 interface ITicketTypeContentProps {
   slug: string;
@@ -14,6 +17,18 @@ interface ITicketTypeContentProps {
 const TicketTypeContent: React.FunctionComponent<ITicketTypeContentProps> = (
   props
 ) => {
+  const user = useAppSelector((state) => state.authState);
+  const router = useRouter()
+
+  const handleBuy = () => {
+    toast.info("Please Login", {
+      description: "You need to login to buy ticket",
+      action:{
+        label: "Login",
+        onClick: () => router.push("/sign-in")
+      }
+    });
+  };
   return (
     <CardContent>
       <div className="border-2 rounded-lg p-4">
@@ -31,10 +46,16 @@ const TicketTypeContent: React.FunctionComponent<ITicketTypeContentProps> = (
           <p>{props.description}</p>
         </div>
         <div className="flex justify-between">
-          <Link href={`/events/${props.slug}/${props.type_name}`}>
-            <Button className="cursor-pointer">Buy</Button>
-          </Link>
           <p className="font-semibold">Quota : {props.quota}</p>
+          {user.email ? (
+            <Link href={`/events/${props.slug}/transaction/${props.type_name}`}>
+              <Button className="cursor-pointer">Buy</Button>
+            </Link>
+          ) : (
+            <Button onClick={handleBuy} className="cursor-pointer">
+              Buy
+            </Button>
+          )}
         </div>
       </div>
     </CardContent>
